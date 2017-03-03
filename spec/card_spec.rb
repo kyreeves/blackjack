36948d51b5f978dee4a66f4b1d4c59e5f34c1172
Kyle Reeves <kyreeves@icloud.com>
2017-03-03 12:34:31 -0600

    add rpsec test for method ace? on card class

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -3,6 +3,7 @@ require "spec_helper"
 RSpec.describe Card do
   let(:non_face_card) { Card.new(rank: 9, suit: '♠') }
   let(:face_card) { Card.new(rank: 'Q', suit: '♦') }
+  let(:ace_card) { Card.new(rank: 'A', suit: '♦') }
 
   it 'has a rank' do
     expect(non_face_card.rank).to eq(9)
@@ -19,6 +20,16 @@ RSpec.describe Card do
 
     it 'returns 10 for a face card' do
       expect(face_card.value).to eq(10)
+    end
+  end
+
+  describe "#ace?" do
+    it 'returns true if rank is an ace' do
+      expect(ace_card.ace?).to eq(true)
+    end
+
+    it 'returns false if rank is not an ace' do
+      expect(face_card.ace?).to eq(false)
     end
   end
 
